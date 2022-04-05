08bc680404353a0a15d6efbea24ab753f1c5fb9e
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-05 12:16:38 +0530

    Single State

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,37 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
-  const [enteredTitle, setenteredTitle] = useState(' ');
-  const [enteredAmount, setenteredAmount] = useState(' ');
-  const [enteredDate, setenteredDate] = useState(' ');
+  // const [enteredTitle, setenteredTitle] = useState(' ');
+  // const [enteredAmount, setenteredAmount] = useState(' ');
+  // const [enteredDate, setenteredDate] = useState(' ');
+
+  const [userInput,setUserInput] = useState({
+    enteredTitle:"",
+    enteredAmount:"",
+    enteredDate:""
+  });
+
   const titleChangeHandler = (event) =>{
-    setenteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle:event.target.value,
+    })
+   // setenteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) =>{
-    setenteredAmount(event.target.value);
-  };
+    setUserInput({
+      ...userInput,
+      enteredAmount:event.target.value,
+  });
+};
 
   const dateChangeHandler = (event) =>{
-    setenteredDate(event.target.value);
-  };
+    setUserInput({
+      ...userInput,
+      enteredDate:event.target.value,
+  });
+};
   return (
     <form>
       <div className='new-expense__controls'>

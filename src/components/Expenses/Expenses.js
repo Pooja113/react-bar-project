a518ce0e7d83e6d22b57201eabddb858d1e0f268
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-05 14:15:34 +0530

    ternary Condition

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,30 +9,23 @@ export default function Expenses(props) {
   const expenseFilterHandler = (selectedYear) => {
     setfilteredYear(selectedYear);
   }
+
+  const filteredExpenses = props.items.filter((expense)=>{
+    return expense.date.getFullYear().toString() === filteredyear;
+  });
+
   return (
     
     <Card className='expenses'>
     <ExpensesFilter selected={filteredyear} onExpenseFilter = {expenseFilterHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+    {filteredExpenses.length === 0 ? <p> No Expenses Found </p>:
+    (filteredExpenses.map((expense) => (<ExpenseItem 
+        key = {expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        />)))
+      }
     </Card>
   )
 }

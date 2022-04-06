84b70f242e4b3374d9183ba64ce930dd3cbab997
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-06 09:42:10 +0530

    Project Done

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,7 +45,7 @@ const submitHandler = (event) => {
 
   const expenseData = {
     title: enteredTitle,
-    amount: enteredAmount,
+    amount: +enteredAmount,
     date: new Date(enteredDate)
   }
   props.onSaveExpenseData(expenseData);

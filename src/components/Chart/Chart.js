d9523d12ba2e66a5820228271e215759653ae70f
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-12 20:23:12 +0530

    chartbar correction

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,15 +4,20 @@ import ChartBar from './ChartBar'
 
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-  const totalMaximum = Math.max(...dataPointValues);
+  const totalMaximum = dataPointValues.reduce(
+    (prevVal, currentValue)=>prevVal+currentValue);
+    console.log(totalMaximum); 
+  //const totalMaximum = Math.max(...dataPointValues);
+  //console.log(totalMaximum);
+
   return (
     <div className='chart'>
     {props.dataPoints.map((dataPoint)=>(
       <ChartBar
-      key = {dataPoint.label} 
-      value={dataPoint.value} 
-      maxValue ={totalMaximum}
-      label = {dataPoint.label}
+          key = {dataPoint.label} 
+          value={dataPoint.value} 
+          maxValue ={totalMaximum}
+          label = {dataPoint.label}
     />
     )
      
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,7 +4,7 @@ import './ChartBar.css'
 const ChartBar = (props) => {
   let barFillHeight = '0%';
   if(props.maxValue > 0){
-    barFillHeight = Math.round((props.value / props.maxValue)*100) + '%';
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
   return (
     <div className='chart-bar'>
@@ -12,10 +12,8 @@ const ChartBar = (props) => {
         <div 
           className='chart-bar__fill' 
           style={{height: barFillHeight }}>
-        
         </div>
-      </div>
-     
+      </div> 
       <div className='chart-bar__label'>{props.label}</div>
     </div>
   )
